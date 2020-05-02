db75f4a88da07cd430992eae71ef7498b3e3b168
Michael Chen <michaelchengaming@gmail.com>
2020-05-02 03:31:23 -0700

    delete unused stuff, change seed data

--- client/src/App.js
+++ client/src/App.js
@@ -6,15 +6,7 @@ import HomePage from "./containers/HomePage";
 // import LoginPage from "./containers/LoginPage";
 import ProfilePage from "./containers/ProfilePage";
 import Dashboard from "./containers/Dashboard";
-import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
-import "@trendmicro/react-sidenav/dist/react-sidenav.css";
-import {
-  faUser,
-  faHouseUser,
-  faUtensils,
-  faSignOutAlt,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SideBar from "./components/SideBar";
 
 function App() {
   const [user, setUser] = useState("");
@@ -52,85 +44,14 @@ function App() {
     <UserContext.Provider value={{ user, token }}>
       <div className="App">
         <Router>
-          <Route
-            render={({ location, history }) => (
-              <React.Fragment>
-                <SideNav
-                  onSelect={(selected) => {
-                    if (!selected) {
-                      console.log(selected);
-                      // LOG OUT FUNCTIONALITY HERE
-                    } else {
-                      const to = "/" + selected;
-                      if (location.pathname !== to) {
-                        history.push(to);
-                      }
-                    }
-                  }}
-                  style={{
-                    backgroundColor: "#282A36",
-                    // #44475A
-                  }}
-                >
-                  <SideNav.Toggle />
-                  <SideNav.Nav defaultSelected="dashboard">
-                    {/* HOME */}
-                    <NavItem eventKey="dashboard">
-                      <NavIcon>
-                        {/* <i className="fa fa-fw fa-home" style={{ fontSize: "1.75em" }} /> */}
-                        <FontAwesomeIcon
-                          icon={faHouseUser}
-                          style={{ fontSize: "1.75em", marginTop: 12 }}
-                        />
-                      </NavIcon>
-                      <NavText>Dashboard</NavText>
-                    </NavItem>
-                    {/* PROFILE */}
-                    <NavItem eventKey="profile">
-                      <NavIcon>
-                        {/* <i
-                          className="fa fa-fw fa-device"
-                          style={{ fontSize: "1.75em" }}
-                        /> */}
-                        <FontAwesomeIcon
-                          icon={faUser}
-                          style={{ fontSize: "1.75em", marginTop: 12 }}
-                        />
-                      </NavIcon>
-                      <NavText>Profile</NavText>
-                    </NavItem>
-                    {/*  */}
-                    <NavItem eventKey="meals">
-                      <NavIcon>
-                        <FontAwesomeIcon
-                          icon={faUtensils}
-                          style={{ fontSize: "1.75em", marginTop: 12 }}
-                        />
-                      </NavIcon>
-                      <NavText>Meals</NavText>
-                    </NavItem>
-                    {/*  */}
-                    <NavItem>
-                      <NavIcon>
-                        <FontAwesomeIcon
-                          icon={faSignOutAlt}
-                          style={{ fontSize: "1.75em", marginTop: 12 }}
-                        />
-                      </NavIcon>
-                      <NavText>Log Out</NavText>
-                    </NavItem>
-                  </SideNav.Nav>
-                </SideNav>
-                <Switch>
-                  <main>
-                    <Route path="/" exact component={HomePage} />
-                    <Route path="/profile" component={ProfilePage} />
-                    <Route path="/dashboard" component={Dashboard} />
-                  </main>
-                </Switch>
-              </React.Fragment>
-            )}
-          />
+          <SideBar theme="#282A36" />
+          <Switch>
+            <main>
+              <Route path="/" exact component={HomePage} />
+              <Route path="/profile" component={ProfilePage} />
+              <Route path="/dashboard" component={Dashboard} />
+            </main>
+          </Switch>
         </Router>
       </div>
     </UserContext.Provider>
