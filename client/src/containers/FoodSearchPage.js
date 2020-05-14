55a9996737043bbea525fb781318d8865a1f847f
Michael Chen <michaelchengaming@gmail.com>
2020-05-13 21:32:17 -0700

    add form and search food to food search page

--- client/src/containers/FoodSearchPage.js
+++ client/src/containers/FoodSearchPage.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
-// const appId = ENV["FOOD_DB_ID"];
 const apiId = process.env.REACT_APP_FOOD_DB_ID;
-// const apiKey = ENV["FOOD_DB_API_KEY"];
 const apiKey = process.env.REACT_APP_FOOD_DB_API_KEY;
 
 const FoodSearchPage = () => {
@@ -9,17 +7,34 @@ const FoodSearchPage = () => {
   const [foodResult, setFoodResult] = useState([]);
 
   useEffect(() => {
-    // update foodResult whenever the search input changes for that good good
+    // update foodResult whenever the search input changes for that good good\
+    fetchFoodInfo();
   }, [search]);
 
   const fetchFoodInfo = async (query = "aunt jemima") => {
     let url = `https://api.edamam.com/api/food-database/parser?ingr=${query}&app_id=${apiId}&app_key=${apiKey}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(search);
   };
 
   return (
     <div>
       hi{apiKey}
       {console.log(process.env)}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e)}
+          placeholder="Enter Food"
+        />
+      </form>
+      <input type="submit" />
     </div>
   );
 };
