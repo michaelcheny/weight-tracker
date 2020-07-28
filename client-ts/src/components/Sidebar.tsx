f40517af5e6579bbec5744fa478bfd598ae2ea17
Michael Chen <michaelchengaming@gmail.com>
2020-07-27 21:25:42 -0700

    add dashboard icon for sidebar, add responsiveness for isdebar items

--- client-ts/src/components/Sidebar.tsx
+++ client-ts/src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import LoginForm from "./LoginForm";
+import DashboardIcon from "@material-ui/icons/Dashboard";
 
 // type
 
@@ -10,13 +11,14 @@ const Sidebar = () => {
     <div className="sidebar">
       {loginShow && <LoginForm showLogin={setLoginShow} />}
       <ul>
+        <Link to="/dashboard" className="link">
+          <DashboardIcon />
+          Dashboard
+        </Link>
+        <Link to="/dashboard" className="link">
+          adsjkfksa
+        </Link>
         <Link to="/" className="link">
-          adsjkfksa
-        </Link>
-        <Link to="/dashboard" className="link">
-          adsjkfksa
-        </Link>
-        <Link to="/dashboard" className="link">
           adsjkfksa
         </Link>
         <p onClick={() => setLoginShow(true)} className="link">
