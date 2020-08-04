13e29884817619ef980d5d82c021216fd37022a4
Michael Chen <michaelchengaming@gmail.com>
2020-08-04 00:12:38 -0700

    fix login form to use reacthookforms, still need to work on errors

--- client-ts/src/components/LoginForm.tsx
+++ client-ts/src/components/LoginForm.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { useClickOutside } from "../helpers/useClickOutside";
 import apiActions from "../helpers/apiActions";
 import { AuthContext } from "../context/AuthContext";
-import Errors from "../components/Errors";
-import {useForm} from 'react-hook-form'
+// import Errors from "../components/Errors";
+import { useForm } from "react-hook-form";
 
 type Input = {
   email: string;
@@ -17,64 +17,36 @@ type LoginFormProps = {
 const LoginForm = ({ showLogin }: LoginFormProps) => {
   const { token, setUser, setAuthenticated } = useContext(AuthContext);
 
-  const { register, handleSubmit, errors } = useForm<Input>()
-  
-  const onSubmit => (data: Input) => {
-    console.log(data)
-  }
+  const { register, handleSubmit, errors } = useForm<Input>();
 
-  // const [input, setInput] = useState<Input>({
-  //   email: "",
-  //   password: "",
-  // });
-
-  // const [error, setError] = useState<boolean>(false);
-  // const [errMsgs, setErrMsgs] = useState<string[] | undefined>([]);
-
-  // const handleLogin = (event: { preventDefault: () => void }) => {
-  //   event.preventDefault();
-  //   console.log(token);
-  //   console.log(input.email);
-  //   console.log(input.password);
-  //   apiActions.logIn(token, input.email, input.password).then((data) => {
-  //     if (!Object.keys(data).includes("errors")) {
-  //       setUser(data);
-  //       setAuthenticated(true);
-  //       showLogin(false);
-  //     } else {
-  //       setError(true);
-  //       setErrMsgs(data.errors);
-  //     }
-  //   });
-  // };
-
-  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>
-  //   setInput({ ...input, [event.target.name]: event.target.value });
+  const onSubmit = (data: Input) => {
+    console.log(data);
+    apiActions.logIn(token, data.email, data.password).then((data) => {
+      if (!Object.keys(data).includes("errors")) {
+        setUser(data);
+        setAuthenticated(true);
+        showLogin(false);
+      }
+    });
+  };
 
   const outsideNode = useClickOutside(() => showLogin(false));
 
   return (
     <div className="form-modal">
-      <form ref={outsideNode} onSubmit={handleSubmit(onsubmit)}>
+      <form ref={outsideNode} onSubmit={handleSubmit(onSubmit)}>
         <h1>Log In</h1>
         {/* {error && <Errors errors={errMsgs} />} */}
         <div>
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            // value={input.email}
-            // onChange={handleChange}
-            required
-          />
+          <input type="text" name="email" placeholder="Email" ref={register({ required: true })} />
+          {errors.email && <span>This field is required</span>}
           <input
             type="password"
             name="password"
             placeholder="Password"
-            // value={input.password}
-            // onChange={handleChange}
-            required
+            ref={register({ required: true })}
           />
+          {errors.password && <span>This field is required</span>}
           <input type="submit" value="Log In" className="submit-button" />
         </div>
       </form>
