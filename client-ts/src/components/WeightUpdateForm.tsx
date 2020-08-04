c1c038bd09827597bae93a3e2051fff5a5eb4701
Michael Chen <michaelchengaming@gmail.com>
2020-08-03 22:31:26 -0700

    add input for weight and submit to the form for weightupdateform

--- client-ts/src/components/WeightUpdateForm.tsx
+++ client-ts/src/components/WeightUpdateForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useClickOutside } from "../helpers/useClickOutside";
 
 type showFormProps = {
@@ -6,10 +6,24 @@ type showFormProps = {
 };
 
 const WeightUpdateForm = ({ showForm }: showFormProps) => {
+  const [newWeight, setNewWeight] = useState<string>();
   const outsideNode = useClickOutside(() => showForm(false));
   return (
     <div className="form-modal">
-      <form ref={outsideNode}></form>
+      <form
+        ref={outsideNode}
+        onSubmit={(e) => {
+          e.preventDefault();
+          // connect to backend and update user weight
+        }}
+      >
+        <input
+          type="text"
+          onChange={(e) => setNewWeight(e.target.value)}
+          value={newWeight}
+          placeholder="How much do you weigh today?"
+        />
+      </form>
     </div>
   );
 };
