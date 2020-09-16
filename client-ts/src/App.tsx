cde1c34330dcf9d7b06d9cb273b8a2a655f63a27
Michael Chen <michaelchengaming@gmail.com>
2020-09-15 23:20:37 -0700

    add route for meals and make mealspage container

--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import LandingPage from "./containers/LandingPage";
-import Dashboard from "./containers/Dashboard";
-import AuthProvider from "./context/AuthContext";
+import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import LandingPage from './containers/LandingPage';
+import Dashboard from './containers/Dashboard';
+import AuthProvider from './context/AuthContext';
 
-import AuthenticatedRoute from "./routes/AuthenticatedRoute";
-import UnauthenticatedRoute from "./routes/UnauthenticatedRoute";
+import AuthenticatedRoute from './routes/AuthenticatedRoute';
+import UnauthenticatedRoute from './routes/UnauthenticatedRoute';
+import MealsPage from './containers/MealsPage';
 
 const App: React.FC = () => {
   return (
@@ -20,6 +21,7 @@ const App: React.FC = () => {
             <Switch>
               <UnauthenticatedRoute path="/" exact component={LandingPage} />
               <AuthenticatedRoute path="/dashboard" component={Dashboard} />
+              <AuthenticatedRoute path="/meals" component={MealsPage} />
             </Switch>
           </div>
         </div>
---
+++ client-ts/src/containers/MealsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MealsPage = () => {
+  return (
+    <div>
+      HELLO
+      <h1>hisdhfisdhf</h1>
+    </div>
+  );
+};
+
+export default MealsPage;
