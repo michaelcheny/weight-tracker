6a99d3991b4cf30efd1557ff665f90a9eaddd17f
Michael Chen <michaelchengaming@gmail.com>
2020-09-15 23:41:58 -0700

    add context to mealspage and set up should i eat button

--- client-ts/src/containers/MealsPage.tsx
+++ client-ts/src/containers/MealsPage.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 const MealsPage = () => {
+  const [yesEat, setYesEat] = useState(true);
+  const { user } = useContext(AuthContext);
   return (
-    <div>
-      HELLO
-      <h1>hisdhfisdhf</h1>
-    </div>
+    <>
+      <section className="container">
+        testing
+        <h1>sdkjfhskdjf</h1>
+        <div className="stat-container">
+          <button
+            onClick={() => {
+              setYesEat((prev) => !prev);
+            }}
+          >
+            Should I Eat This?
+          </button>
+        </div>
+      </section>
+    </>
   );
 };
 
