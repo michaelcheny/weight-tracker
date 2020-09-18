f351fd89b4f8ab60b3b1c04585b290264b87d617
Michael Chen <michaelchengaming@gmail.com>
2020-09-17 23:56:49 -0700

    add axios

--- client-ts/src/containers/MealsPage.tsx
+++ client-ts/src/containers/MealsPage.tsx
@@ -1,7 +1,10 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+const axios = require('axios');
 
-let URLMAIN = `https://api.edamam.com/api/food-database/v2/parser?ingr=red%20apple`;
+axios.defaults.baseURL = `https://api.edamam.com/api/food-database/v2/parser?ingr=red%20apple`;
+
+// let URLMAIN = `https://api.edamam.com/api/food-database/v2/parser?ingr=red%20apple`;
 const URLAUTH = `&app_id=${process.env.REACT_APP_FOOD_DB_ID}&app_key=${process.env.REACT_APP_FOOD_DB_API_KEY}`;
 
 const MealsPage = () => {
